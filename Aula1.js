c485eb87407585b4bd70b1d1b4135139f190bc7d
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2024-09-11 09:01:28 -0300

    Add files via upload

---
+++ Aula1.js
@@ -0,0 +1,12 @@
+const texto1 = "Olá mundo";
+const texto2 = "Olá mundo";
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é";
+const meuNome = "Deborah";
+
+console.log(citacao + meuNome);
+
+// concatenacao - juntar duas ou mais variaveis (textos, operadores, variaveis)
+//template string ou template literal
