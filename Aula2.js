9d108834b96a95bb499aee99a902e085b0569398
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2024-09-12 08:09:18 -0300

    JS

---
+++ Aula2.js
@@ -0,0 +1,12 @@
+console.log("Meu primeiro programa: trabalhando com variavei");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idad)
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade/2)
+
+const idadeSomada = idade+2;
+console.log(idadeSomad);
