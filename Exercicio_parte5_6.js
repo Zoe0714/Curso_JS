f04773ee8815667cc91af0e51e51a233100abcae
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2024-10-23 08:34:34 -0300

    Add files via upload

---
+++ Exercicio_parte5_6.js
@@ -0,0 +1,28 @@
+// Exemplo de tipos de dados básicos em JavaScript
+
+// Nome do aluno
+let nome = "Lana";
+console.log("Nome:" + nome);
+
+//Idade do aluno
+let idade = 15;
+console.log("Idade: " + idade);
+
+//Disciplinas matriculadas
+let disciplinas = ["Matematica", "Geografia", "Biologia"];
+console.log("As disciplinas matriculadas são:" + disciplinas);
+
+//Conclusao do curso
+let conclusao = true;
+console.log("Está aprovado" + conclusao);
+
+// Informações mais complexas sobre o aluno
+let aluno = {
+  nome: "Lana",
+  disciplinas: "Matematica, Geografia e Biologia",
+  idade: 15,
+  concluiuCurso: true,
+  notaFinal: 100
+};
+console.log("Informações sobre o aluno");
+console.log(aluno);
